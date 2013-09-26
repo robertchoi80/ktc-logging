08a500dcb3e1b650671c6f7fc6b9aa97005fde75
Robert <taeil.choi@kt.com>
2013-09-26 14:30:32 +0900

    change logstash download url and index retention period

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,11 +4,11 @@ override[:logstash][:splunk_port] = ''
 
 override[:logstash][:server][:version] = '1.2.1'
 override[:logstash][:server][:source_url] =
-  'https://logstash.objects.dreamhost.com/release/logstash-1.2.1-flatjar.jar'
+  'https://download.elasticsearch.org/logstash/logstash/logstash-1.2.1-flatjar.jar'
 override[:logstash][:server][:inputs] = [:syslog => { :type=>'syslog', :port=>'5514' }]
 override[:logstash][:server][:outputs] = []
 
-override[:logstash][:index_cleaner][:days_to_keep] = 7
+override[:logstash][:index_cleaner][:days_to_keep] = 28
 override[:logstash][:index_cleaner][:cron][:minute] = '0'
 override[:logstash][:index_cleaner][:cron][:hour] = '1'
 override[:logstash][:index_cleaner][:cron][:log_file] = '/dev/null'

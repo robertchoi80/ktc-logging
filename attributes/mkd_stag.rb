fb1b8806c44f1f2a4f97af8ae7a4ef4c86c2dfef
Robert <taeil.choi@kt.com>
2013-10-28 13:33:15 +0900

    add mkd-stag attr

---
+++ attributes/mkd_stag.rb
@@ -0,0 +1,31 @@
+#
+# Attributes for mkd_stag environment
+#
+return unless chef_environment == "mkd_stag"
+
+include_attribute "ktc-logging::default"
+
+default[:logging][:recipes_server_logstash] = %w{
+  ktc-logging::logstash
+}
+
+default[:logging][:recipes_server_es] = %w{
+  java
+  elasticsearch::default
+  logstash::index_cleaner
+  kibana::default
+}
+
+default[:logging][:recipes_server_kibana] = %w{
+}
+
+default[:logging][:recipes_client] = %w{
+  ktc-rsyslog::default
+}
+
+# Logstash attributes
+default[:logstash][:elasticsearch_cluster] = 'es-cluster-mkd-stag'
+default[:elasticsearch][:cluster][:name] = 'es-cluster-mkd-stag'
+
+# TODO: This endpoint should be handled with Services library in ktc-rsyslog.
+default[:rsyslog][:logstash_server] = 'logstash01-vm.mkd-stag'

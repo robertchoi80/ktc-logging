40afe683a64cc209044457527fa5fa351c176b0b
ktc-jenkins <jenkins.ktcloudware@gmail.com>
2013-12-11 15:49:53 +0900

    auto_version to 1.0.15

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "ktc-logging"
 maintainer        "Robert Choi"
 license           "Apache 2.0"
 description       "TLC for logging"
-version '1.0.12'
+version '1.0.15'
 
 depends "elasticsearch"
 depends "kibana"

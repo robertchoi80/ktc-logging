46396cf86cd7d1fb95fc8103822453ec0d492f44
Robert <taeil.choi@kt.com>
2014-01-19 20:33:08 +0900

    rewind config.js template

--- recipes/kibana.rb
+++ recipes/kibana.rb
@@ -1,6 +1,17 @@
+chef_gem "chef-rewind"
+require 'chef/rewind'
+
 es_server_results = search(:node, node['logging']['elasticsearch_query'])
 unless es_server_results.empty?
   node.set['kibana']['es_server'] = es_server_results[0]['ipaddress']
 end
 
 include_recipe "kibana"
+
+rewind :template => "#{node['kibana']['web_dir']}/config.js" do
+  source "config-new.js.erb"
+  cookbook "ktc-logging"
+  variables(:es_host => node['kibana']['es_server'],
+    :es_port => node['kibana']['es_port']
+  )
+end
